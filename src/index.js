fc8835d9cec11333d8eec476cdac00bc6db61d55
Joshua Reyes <reyes.j15@gmail.com>
2022-02-24 05:33:33 -0500

    phase 5 to be continues

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('hello world');
+const ul = document.querySelector('ul');
+ul.addEventListener('click', async (ev) => {
+    if(ev.target.tagName === 'LI') {
+        const id = ev.target.getAttribute('data-id');
+        await axios.delete(`/api/artists/${id}`);
+        // init();
+    }
+});
+const init = async()=>{
+    const response = await axios.get('/api/artists');
+    console.log(response);
+    const artists = response.data;
+    const html = artists.map( artist => {
+        return `
+            <li data-id='${artist.id}'>${artist.name}</li>
+        
+        `;
+    }).join('');
+    ul.innerHTML = html;
+};
+
+init();
