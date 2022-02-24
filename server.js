31edf5afc8d18ed95d67bd2814026092c91893f8
Joshua Reyes <reyes.j15@gmail.com>
2022-02-24 03:48:19 -0500

    phase 1 update phase 2 complete

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
+const { append } = require('express/lib/response');
 const { user } = require('pg/lib/defaults');
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/dealers_choice_spa'); //set up sequelize instance
 const STRING = Sequelize.DataTypes.STRING;
+const express = require('express');
+const app = new express();
 
 const Artist = sequelize.define('artist', {
     name: {
-        type: String,
-        unique: true
+        type: STRING,
+    }
+});
+//ph2 create a GET routes
+app.get('/api/artists', async(req,res,next)=>{
+    try{
+        res.send(await Artist.findAll());
+    }
+    catch(ex){
+        next(ex);
     }
 });
 
@@ -17,7 +28,8 @@ const init = async() => {
         await Artist.create({name: 'Drake'});
         await Artist.create({name: 'The Weeknd'});
         await Artist.create({name: 'Taylor Swift'});
-
+        const port = process.env.PORT || 3000;
+        app.listen(port, ()=> console.log(`istening on port ${port}`))
     }
     catch(ex){
         console.log(ex);
